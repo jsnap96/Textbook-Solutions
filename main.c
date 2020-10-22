345a1d7ced2e0b61491543100ce976e1d74dd1fc
jsnap96 <61860100+jsnap96@users.noreply.github.com>
2020-10-22 18:04:05 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,39 @@
+//
+//  main.c
+//  Proj1
+//
+//  Created by Joe Ortiz on 6/9/20.
+//  Copyright © 2020 Joe Ortiz. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+//Chapter 2 Q4
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Enter an amount:");
+    
+    double amount;
+    
+    scanf("%lf",&amount);
+    
+    double tax = 0.05;
+    
+    double add_tax = amount * tax;
+    
+    double display;
+    
+    display = add_tax+amount;
+    
+    printf("With tax added: $ %0.2lf\n",display);
+  
+    
+    
+    
+    
+    
+    
+    return 0;
+}
